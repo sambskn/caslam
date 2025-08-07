1aecf3972ca9bc6ee8f1925a9cbbf5199bba9f28
Sam Baskin <sambskn@gmail.com>
2025-08-06 23:43:00 -0600

    better number picking, cooler hexes

--- source/main.lua
+++ source/main.lua
@@ -27,29 +27,31 @@ local function getHexImage(height, hexType, hexNum)
     gfx.pushContext(hexImg)
     -- shoutout https://gurgleapps.com/tools/matrix
     if hexType == 1 then
-        -- dark herringbone
-        gfx.setPattern({ 0x5a, 0xa5, 0x5a, 0xa5, 0x5a, 0xa5, 0x5a, 0xa5 })
+        -- plant???
+        gfx.setPattern({ 0xad, 0x8e, 0x3f, 0x1f, 0xbb, 0xf1, 0x7b, 0x2f })
     end
     if hexType == 2 then
-        -- stripe
-        gfx.setPattern({ 0xfc, 0xf9, 0xf3, 0xe7, 0xcf, 0x9f, 0x3f, 0x7e })
+        -- stripe grass
+        gfx.setPattern({ 0xdf, 0x75, 0xee, 0xbf, 0xeb, 0x75, 0xfe, 0xab })
     end
     if hexType == 3 then
         -- dots
-        gfx.setPattern({ 0xdd, 0x77, 0xdd, 0x77, 0xdd, 0x77, 0xdd, 0x77 })
+        gfx.setPattern({ 0xfd, 0xdf, 0xf7, 0x7f, 0xfd, 0xdf, 0xf7, 0x7f })
     end
     if hexType == 4 then
-        --mesh
-        gfx.setPattern { 0xda, 0xba, 0xba, 0xfa, 0xd7, 0x57, 0x5d, 0x5d }
+        --flower
+        gfx.setPattern { 0x5a, 0xdb, 0x3c, 0xe7, 0xe7, 0x3c, 0xdb, 0x5a }
     end
     if hexType == 5 then
         --brick
         gfx.setPattern { 0x3f, 0x9f, 0xcf, 0xc7, 0x93, 0x39, 0x7c, 0x7e }
     end
     if hexType == 6 then
-        --flower
-        gfx.setPattern { 0x5a, 0xdb, 0x3c, 0xe7, 0xe7, 0x3c, 0xdb, 0x5a }
+        --mesh
+        gfx.setPattern { 0x3b, 0xb5, 0x67, 0x6b, 0xcc, 0xb2, 0xad, 0x4b }
     end
+
+
     gfx.fillPolygon(
         0, vertCornerOffset,
         hexWidth / 2, 0,
@@ -70,10 +72,15 @@ local function getHexImage(height, hexType, hexNum)
         0, vertCornerOffset + hexagonSide,
         0, vertCornerOffset
     )
-    gfx.fillRect(hexWidth * 9 / 24, height * 9 / 24, hexWidth * 5 / 24, height / 6)
-    gfx.setColor(gfx.kColorBlack)
-    gfx.setFont(numFont)
-    gfx.drawTextAligned(hexNum, hexWidth * 5 / 12, height * 5 / 12, gfx.kAlignCenter)
+    -- only draw number if not desert
+    if hexType ~= 6 then
+        gfx.setFont(numFont)
+        local textWidth = gfx.getTextSize(hexNum)
+        gfx.fillRect(hexWidth * 1 / 2 - textWidth / 2 - 2, height * 9 / 24, textWidth + 4, height / 6)
+        gfx.setColor(gfx.kColorBlack)
+        gfx.drawTextAligned(hexNum, hexWidth * 1 / 2 - textWidth / 2, height * 5 / 12, gfx.kAlignCenter)
+    end
+
     gfx.popContext()
     return hexImg
 end
@@ -97,7 +104,9 @@ local function drawIndicator()
     gfx.fillCircleAtPoint(indicatorX, indicatorY, bumpRad)
 end
 
-local hexRows = { { 1, 2, 3 }, { 4, 5, 6, 1 }, { 3, 4, 5, 6, 1 }, { 3, 2, 1, 4 }, { 2, 2, 5 } }
+local hexRows = { { 1, 2, 3 }, { 4, 5, 3, 1 }, { 3, 4, 2, 6, 1 }, { 3, 5, 4, 2 }, { 2, 1, 5 } }
+
+local numberSet = { 5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11 }
 
 local targetIndicatorX = indicatorX
 local targetIndicatorY = indicatorY
@@ -180,9 +189,10 @@ function playdate.update()
     local hexagonSide = hexHeight / 2
     local hexWidth = math.sqrt(3) * hexagonSide
     local vertCornerOffset = (hexHeight - hexagonSide) / 2
+    local hexIndex = 1
     for row, hexesTable in pairs(hexRows) do
         for col, hexType in pairs(hexesTable) do
-            local newHex = getHexImage(hexHeight, hexType, (row + col) % 12 + 1)
+            local newHex = getHexImage(hexHeight, hexType, numberSet[hexIndex])
             local x = originX + (hexWidth * col) - (hexWidth / 2)
             if row % 2 == 0 then
                 x -= hexWidth / 2
@@ -192,6 +202,10 @@ function playdate.update()
             end
             local y = originY + row * (hexHeight - vertCornerOffset)
             newHex:drawAnchored(x, y, 0.0, 0.0)
+            -- skip desert, it doesn't get its number used
+            if hexType ~= 6 then
+                hexIndex += 1
+            end
         end
     end
     drawIndicator()
